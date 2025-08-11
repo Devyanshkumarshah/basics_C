113ed7b35bc829e7e2e3e3f3523d1b56d9fdd781
Devyanshkumarshah <144312351+Devyanshkumarshah@users.noreply.github.com>
2025-08-11 20:53:37 +0530

    pointer.c

--- pointer.c
+++ pointer.c
@@ -10,3 +10,23 @@ int main()
 
     return 0;
 }
+
+output - &abc: 0061FF1B, ptr: 0061FF1B, sizeof(ptr): 4
+
+
+
+/*sum of elements of the array*/
+
+#include<stdio.h>
+int main()
+{
+    int a[] = {11,2,36,4,90,81};
+    int sum = 0, *p;
+
+    for(p = &a[0]; p <= &a[4]; p++){
+        sum += *p;
+    }
+    printf("sum of the array is %d", sum);
+}
+
+output - sum of the array is 143
